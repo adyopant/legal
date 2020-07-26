f5623cdac81bd318f587b603ca4bda55ab81c808
Nat Grant <email.natgrant@gmail.com>
2020-07-26 19:58:45 +1200

    Wrap app component in xstate machine, introduce private route to home

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React, { Fragment } from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 // Components
 import Home from "./components/Home";
 import Nav from "./components/Nav";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import PrivateRoute from "./components/privateroute";
+
+// State
+import { useMachine } from "@xstate/react";
+import { appMachine, MachineContext } from "./state";
 
 export default function App() {
+  const [currentMachine, sendToMachine] = useMachine(appMachine);
+
   return (
-    <Fragment>
+    <MachineContext.Provider value={[currentMachine, sendToMachine]}>
       <Router>
-        <Nav />
-        <Route exact path="/" component={Home} />
-        <Route path="/sign-in" component={Login} />
-        <Route path="/register" component={Register} />
+        <div className="App">
+          <Nav />
+          <Switch>
+            <Route path="/sign-in" component={Login} />
+            <Route path="/register" component={Register} />
+            <PrivateRoute machine={currentMachine}>
+              <Route exact path="/" component={Home} />
+            </PrivateRoute>
+          </Switch>
+        </div>
       </Router>
-    </Fragment>
+    </MachineContext.Provider>
   );
 }
